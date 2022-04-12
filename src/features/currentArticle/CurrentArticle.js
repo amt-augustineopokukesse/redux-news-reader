8f56206f304bcc7237b751e1dbec474b6f2cb1d3
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-12 09:27:29 +0100

    Fix errors

--- src/features/currentArticle/CurrentArticle.js
+++ src/features/currentArticle/CurrentArticle.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import {
   selectCurrentArticle,
   isLoadingCurrentArticle,
@@ -7,7 +7,7 @@ import {
 import FullArticle from '../../components/FullArticle';
 
 const CurrentArticle = () => {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   const article = useSelector(selectCurrentArticle);
   const currentArticleIsLoading = useSelector(isLoadingCurrentArticle);
 
